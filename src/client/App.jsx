abafe7ff7e35c7ae5fb78cb43c1094686b1ad9e2
“tyler-e-spicer” <tyler.e.spicer@gmail.com>
2023-12-18 18:05:16 -0500

    react router paths

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <p>hi</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
