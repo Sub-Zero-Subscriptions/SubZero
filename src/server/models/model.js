8cb5a4db8ea0367d772b1cd412c1e2ef4c57cc69
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-18 15:41:55 -0800

    add client in model.js

--- src/server/models/model.js
+++ src/server/models/model.js
@@ -1,21 +1,18 @@
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
 const PG_URI = 'postgres://';
 
-// create a new pool here using the connection string above
-const pool = new Pool({
+// create a new client here using the connection string above
+const client = new Client({
   connectionString: PG_URI
 });
 
-// Schema for the database can be found below:
-// INSERT SCHEMA HERE
+client.connect();
 
-// We export an object that contains a property called query,
-// which is a function that returns the invocation of pool.query() after logging the query
-// This will be required in the controllers to be the access point to the database
+
 module.exports = {
   query: (text, params, callback) => {
     console.log('executed query', text);
-    return pool.query(text, params, callback);
+    return client.query(text, params, callback);
   }
 };
