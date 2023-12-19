13de9058f7305b8770f3c6120fec37df3d964f7b
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-19 13:28:16 -0800

    add authController hash and jwt function

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -2,18 +2,81 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// importing postgres uri and creating pool as temporary solution. Later will need to import the file where this pool is created
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URI
 })
+// async function test() {
+//     const result = await pool.query('SELECT * FROM subscriptions')
+//     console.log(result.rows);    
+// }
+
+// test();
 
 const SALT_WORK_FACTOR = 10;
 
 const authcontroller = {};
 
+//generates a jwt
+function createToken(id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRATION});
+}
 
-// authcontroller.signup = 
+//generates a hashed password
+function hashPassword(password) {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, SALT_WORK_FACTOR, (err, hash) => {
+            if(err) {
+                reject({
+                    log: `Error hashing password`,
+                    status: 500,
+                    message: { err: `Error in signup`},
+                    });
+            } else {
+               resolve(hash) 
+            }
+        })
+    })
+}
+//controller for a new user signing up
+authcontroller.signup = async (req, res, next) => {
+    console.log('signup controller invoked');
+    try {
+        //get username and password from req.body. If either are falsy, invoke global error handler
+        const {username, password} = req.body;
+        if(!username || !password) {
+            return next(
+                {
+                    log: 'Username or password not submitted', 
+                    status: 422,
+                    message: { err: 'Username or password not submitted' },
+                }
+            )
+        }
+        //check if username already exists in DB
+        const selectValues = [username];
+        const selectQuery = `
+        
+        `;
+        const userDetails = await pool.query(selectQuery, selectValues);
+        if(userDetails.rowCount > 0) {
+            return next(
+                {
+                    log: 'Username already exists',
+                    status: 409,
+                    message: { err: 'Username already exists' },        
+                }
+            )
+        }
+    }
+    catch{
+        next({
+            log: `Express error handler caught middleware error in authcontroller.signup. Error: ${error}`,
+            status: 500,
+            message: { err: `Error in signup: ${error}`},
+        })
+    }
+}
 
 //authcontroller.login = 
 
