d2b46fbb95a1c85ff899530afb255f2bd835971d
Dana Kaplan <danakaplan96@gmail.com>
2023-12-19 15:08:47 -0500

    chart component

--- src/client/components/Dashboard.jsx
+++ src/client/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import SubscriptionSection from './SubscriptionSection';
+import SubscriptionChart from './SubscriptionChart';
 
 export default function Dashboard() {
   return (
     <div>
       <SubscriptionSection />
+      <SubscriptionChart />
     </div>
   );
 }
---
+++ src/client/components/SubscriptionChart.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const SubscriptionChart = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    // Dummy data for subscriptions and their costs
+    const data = {
+      labels: ['Netflix $10', 'Spotify $5', 'Hulu $8', 'Amazon Prime $12'],
+      datasets: [
+        {
+          data: [10, 5, 8, 12], // Dummy subscription costs
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50'],
+        },
+      ],
+    };
+
+    // Get the chart context from the canvas reference
+    const ctx = chartRef.current.getContext('2d');
+
+    // Destroy existing chart instance if it exists
+    if (chartRef.current.chart) {
+      chartRef.current.chart.destroy();
+    }
+
+    // Create a doughnut chart
+    chartRef.current.chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'Subscription Costs',
+        },
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={chartRef} width='400' height='400'></canvas>
+    </div>
+  );
+};
+
+export default SubscriptionChart;
