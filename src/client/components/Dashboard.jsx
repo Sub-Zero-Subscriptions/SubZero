918e895d6768a953da6b2a0a71746aca332f1ab2
Dana Kaplan <danakaplan96@gmail.com>
2023-12-19 17:36:21 -0500

    state and components

--- src/client/components/Dashboard.jsx
+++ src/client/components/Dashboard.jsx
@@ -1,11 +1,27 @@
-import SubscriptionSection from './SubscriptionSection';
+import React from 'react';
+import { Grid } from '@mui/material';
 import SubscriptionChart from './SubscriptionChart';
+import SubscriptionForm from './SubscriptionForm';
+import SubscriptionList from './SubscriptionList';
 
 export default function Dashboard() {
   return (
-    <div>
-      <SubscriptionSection />
-      <SubscriptionChart />
-    </div>
+    <Grid container style={{ width: '90%', margin: 'auto' }}>
+      <Grid item xs={12} lg={4}>
+        <div style={{ padding: '1rem' }}>
+          <SubscriptionList />
+        </div>
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <div style={{ padding: '1rem' }}>
+          <SubscriptionForm />
+        </div>
+      </Grid>
+      <Grid item xs={12} lg={4}>
+        <div style={{ padding: '1rem' }}>
+          <SubscriptionChart />
+        </div>
+      </Grid>
+    </Grid>
   );
 }
--- src/client/components/SubscriptionForm.jsx
+++ src/client/components/SubscriptionForm.jsx
@@ -1,57 +1,101 @@
-// SubscriptionFormView.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addSubscription,
+  editSubscription,
+} from '../store/slices/subscriptionSlice';
 
-const SubscriptionForm = ({
-  subscription,
-  onInputChange,
-  onAddSubscription,
-  isEditing,
-}) => (
-  <div>
-    {/* Display the title based on whether it's an edit or add operation */}
-    <Typography variant='h5' gutterBottom>
-      {isEditing ? 'Edit Subscription' : 'Add Subscription'}
-    </Typography>
-    <Paper elevation={3} style={{ padding: '20px', marginBottom: '20px' }}>
-      <TextField
-        label='Subscription Name'
-        fullWidth
-        margin='normal'
-        value={subscription.name}
-        onChange={(e) => onInputChange('name', e.target.value)}
-      />
-      <TextField
-        label='Subscription Purchase Date'
-        fullWidth
-        margin='normal'
-        value={subscription.purchaseDate}
-        onChange={(e) => onInputChange('purchaseDate', e.target.value)}
-      />
-      <TextField
-        label='Subscription End Date'
-        fullWidth
-        margin='normal'
-        value={subscription.endDate}
-        onChange={(e) => onInputChange('endDate', e.target.value)}
-      />
-      <TextField
-        label='Amount Charged $'
-        fullWidth
-        margin='normal'
-        value={subscription.amountCharged}
-        onChange={(e) => onInputChange('amountCharged', e.target.value)}
-      />
-      <Button
-        variant='contained'
-        color='primary'
-        onClick={onAddSubscription}
-        style={{ marginTop: '10px' }}
-      >
-        {isEditing ? 'Save Changes' : 'Add Subscription'}
-      </Button>
-    </Paper>
-  </div>
-);
+export default function SubscriptionForm() {
+  const dispatch = useDispatch();
+  const { isEditMode, selectedSubscription } = useSelector(
+    (state) => state.subscriptionState
+  );
 
-export default SubscriptionForm;
+  const [subscription, setSubscription] = useState({
+    name: '',
+    purchaseDate: '',
+    endDate: '',
+    amountCharged: '',
+  });
+
+  const handleSubmit = () => {
+    if (isEditMode) {
+      console.log('EDIT');
+      dispatch(editSubscription(subscription));
+    } else {
+      console.log('NEW');
+      dispatch(addSubscription(subscription));
+    }
+    resetSubscription();
+  };
+
+  const handleInputChange = (key, value) => {
+    const clone = { ...subscription };
+    clone[key] = value;
+    setSubscription(clone);
+  };
+
+  const resetSubscription = () =>
+    setSubscription({
+      name: '',
+      purchaseDate: '',
+      endDate: '',
+      amountCharged: '',
+    });
+
+  useEffect(() => {
+    if (isEditMode) {
+      setSubscription(selectedSubscription);
+    } else {
+      resetSubscription();
+    }
+  }, [isEditMode]);
+
+  return (
+    <div>
+      {/* Display the title based on whether it's an edit or add operation */}
+      <Typography variant='h5' gutterBottom>
+        {isEditMode ? 'Edit Subscription' : 'Add Subscription'}
+      </Typography>
+      <Paper elevation={3} style={{ padding: '20px', marginBottom: '20px' }}>
+        <TextField
+          label='Subscription Name'
+          fullWidth
+          margin='normal'
+          value={subscription.name}
+          onChange={(e) => handleInputChange('name', e.target.value)}
+        />
+        <TextField
+          label='Subscription Purchase Date'
+          fullWidth
+          margin='normal'
+          value={subscription.purchaseDate}
+          onChange={(e) => handleInputChange('purchaseDate', e.target.value)}
+        />
+        <TextField
+          label='Subscription End Date'
+          fullWidth
+          margin='normal'
+          value={subscription.endDate}
+          onChange={(e) => handleInputChange('endDate', e.target.value)}
+        />
+        <TextField
+          label='Amount Charged $'
+          fullWidth
+          margin='normal'
+          value={subscription.amountCharged}
+          onChange={(e) => handleInputChange('amountCharged', e.target.value)}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={handleSubmit}
+          style={{ marginTop: '10px' }}
+        >
+          {isEditMode ? 'Save Changes' : 'Add Subscription'}
+        </Button>
+      </Paper>
+    </div>
+  );
+}
