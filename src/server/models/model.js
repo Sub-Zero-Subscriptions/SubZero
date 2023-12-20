d5ca74f79befd05379fceafdfe8f3555f29e6635
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-20 12:38:00 -0800

    changed client to pool in model

--- src/server/models/model.js
+++ src/server/models/model.js
@@ -1,26 +1,42 @@
 import pg from 'pg';
-//const { Client } = require('pg');
 
-const PG_URI =
-  'postgres://yysmplmk:55twtSJzsQp2o_0wW3aHRHxBl1QQPELU@bubble.db.elephantsql.com/yysmplmk';
+const PG_URI = 'postgres://yysmplmk:55twtSJzsQp2o_0wW3aHRHxBl1QQPELU@bubble.db.elephantsql.com/yysmplmk';
 
-//create a new client here using the connection string above
-const client = new pg.Client({
+// Create a new pool here using the connection string above
+const pool = new pg.Pool({
   connectionString: PG_URI,
 });
 
-client.connect();
-
 export default {
-  query: async (text, params, callback) => {
-    console.log('executed query', text);
-    return client.query(text, params, callback);
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
   },
 };
 
-// module.exports = {
-//   query: (text, params, callback) => {
+
+// import pg from 'pg';
+// //const { Client } = require('pg');
+
+// const PG_URI =
+//   'postgres://yysmplmk:55twtSJzsQp2o_0wW3aHRHxBl1QQPELU@bubble.db.elephantsql.com/yysmplmk';
+
+// //create a new client here using the connection string above
+// const client = new pg.Client({
+//   connectionString: PG_URI,
+// });
+
+// client.connect();
+
+// export default {
+//   query: async (text, params, callback) => {
 //     console.log('executed query', text);
 //     return client.query(text, params, callback);
 //   },
 // };
+
+// // module.exports = {
+// //   query: (text, params, callback) => {
+// //     console.log('executed query', text);
+// //     return client.query(text, params, callback);
+// //   },
+// // };
