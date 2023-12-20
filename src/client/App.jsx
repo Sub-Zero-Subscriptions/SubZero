67786e5f957671d42bdc0638e77434a0bcd43c79
Dana Kaplan <danakaplan96@gmail.com>
2023-12-19 19:04:24 -0500

    merged

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <Router>
-      <NavBar />
       <Routes>
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/' element={<Login />} />
--- src/client/components/Dashboard.jsx
+++ src/client/components/Dashboard.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import NavBar from './NavBar';
 import SubscriptionChart from './SubscriptionChart';
 import SubscriptionForm from './SubscriptionForm';
 import SubscriptionList from './SubscriptionList';
 
 export default function Dashboard() {
   return (
-    <Grid container style={{ width: '90%', margin: 'auto' }}>
-      <Grid item xs={12} lg={4}>
-        <div style={{ padding: '1rem' }}>
-          <SubscriptionList />
-        </div>
+    <div>
+      <NavBar />
+      <Grid container style={{ width: '90%', margin: 'auto' }}>
+        <Grid item xs={12} lg={4}>
+          <div style={{ padding: '1rem' }}>
+            <SubscriptionList />
+          </div>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <div style={{ padding: '1rem' }}>
+            <SubscriptionForm />
+          </div>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <div style={{ padding: '1rem' }}>
+            <SubscriptionChart />
+          </div>
+        </Grid>
       </Grid>
-      <Grid item xs={12} lg={4}>
-        <div style={{ padding: '1rem' }}>
-          <SubscriptionForm />
-        </div>
-      </Grid>
-      <Grid item xs={12} lg={4}>
-        <div style={{ padding: '1rem' }}>
-          <SubscriptionChart />
-        </div>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
