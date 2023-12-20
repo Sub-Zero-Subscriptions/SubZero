c0d93e13c76c9e0cdb57e2e27daba2b1496f4f58
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-12-19 17:58:38 -0800

    add Providor and store import to main.jsx

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -5,7 +5,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import { Provider } from 'react-redux';
+import {store} from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
